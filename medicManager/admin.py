a5a3a47a0faa7b8680e05c15ccb64739308f062b
pedrofhasler <pedro.hasler@gmail.com>
2022-09-28 23:05:19 -0300

    feat: Adding some model to Django Admin

--- medicManager/admin.py
+++ medicManager/admin.py
@@ -2,4 +2,13 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-admin.site.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+
+    date_hierarchy = "created_at"
+
+
+admin.site.register(Profile, ProfileAdmin)
+admin.site.register(Address)
+admin.site.register(DayWeek)
+admin.site.register(Rating)
+admin.site.register(Speciality)
